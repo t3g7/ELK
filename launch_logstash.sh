25a348a23761d5c05bb3634125f08af58f7a3212
fhorel <fhorel@enseirb.fr>
2015-11-30 10:46:57 +0100

    Create first dashboard with screenshot

--- launch_logstash.sh
+++ launch_logstash.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#kill $LOGSTASH_PID
+
 curl -XDELETE localhost:9200/logstash-twitter-bis
 
 ./logstash-1.5.0/bin/logstash -f tweettxt.conf &
+
+#LOGSTASH_PID=$! 
+#export LOGSTASH_PID
